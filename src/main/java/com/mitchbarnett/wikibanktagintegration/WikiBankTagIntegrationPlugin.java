2d60bcece12bf8f13fce432373cc7a5de37de00d
Mitch Barnett <mitch@mitchbarnett.com>
2020-05-26 22:19:37 +0100

    Basic project structure

--- src/main/java/com/mitchbarnett/wikibanktagintegration/WikiBankTagIntegrationPlugin.java
+++ src/main/java/com/mitchbarnett/wikibanktagintegration/WikiBankTagIntegrationPlugin.java
@@ -10,6 +10,13 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.http.api.RuneLiteAPI;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 
 @Slf4j
@@ -54,6 +61,58 @@ public class WikiBankTagIntegrationPlugin extends Plugin
 	private void addTagsFromCategory(String category)
 	{
 		log.info("attempting to add tags to items from " + category);
+
+		List<Integer> items = getCategoryIDs(category);
+
+		tagItems(items, category);
+
+		String message = "Added " + category + " tag to " + String.valueOf(items.size()) + " items.";
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE,"", message,"");
+		createTab(category);
 	}
+
+	private void tagItems(List<Integer> items, String category)
+	{
+	}
+
+	private void createTab(String category)
+	{
+	}
+
+
+	private List<Integer> getCategoryIDs(String category)
+	{
+		try
+		{
+			String wikiResponse = getWikiResponse(category).body().string();
+			return getIDsFromJSON(wikiResponse);
+		}
+		catch (IOException e)
+		{
+			log.error(e.getMessage());
+			return Collections.emptyList();
+		}
+	}
+
+	private Response getWikiResponse(String category) throws IOException {
+		Request request = new Request.Builder()
+				.url(createQueryURL(category))
+				.build();
+
+		return RuneLiteAPI.CLIENT.newCall(request).execute();
+	}
+
+
+	String createQueryURL(String category)
+	{
+		return "https://oldschool.runescape.wiki/api.php?action=ask&query=[[Category:]]" + category + "|?All+Item+ID&fpr,at=json";
+	}
+
+	List<Integer> getIDsFromJSON(String Json)
+	{
+		return Collections.emptyList();
+	}
+
+
 }
 
